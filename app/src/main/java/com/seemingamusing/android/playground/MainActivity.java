112e8b25eb88e1702d22318607d467f0f7a9c88e
Eric Lee <eric.m.lee@deliveryhero.com>
2016-03-18 15:06:16 +0100

    Update MainActivity to launch sample activities
    This commit removes the previous implementation with the feature of listing all
    sample activities available to try out.

--- app/src/main/java/com/seemingamusing/android/playground/MainActivity.java
+++ app/src/main/java/com/seemingamusing/android/playground/MainActivity.java
@@ -1,53 +1,76 @@
 package com.seemingamusing.android.playground;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.FrameLayout;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
-import com.seemingamusing.android.playground.common.MockedDataAdapter;
+import com.seemingamusing.android.playground.footer.FooterActivity;
+import com.seemingamusing.android.playground.model.SampleActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Bind(R.id.toolbar) Toolbar mToolbar;
-  @Bind(R.id.content_view) RecyclerView mContentView;
-  @Bind(R.id.footer_bar) FrameLayout mFooterBar;
+  @Bind(R.id.samples_listing) RecyclerView mSamples;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_footer);
+    setContentView(R.layout.activity_main);
     ButterKnife.bind(this);
-    setSupportActionBar(mToolbar);
     initializeContentView();
   }
 
   private void initializeContentView() {
-    mContentView.setLayoutManager(new LinearLayoutManager(this));
-    mContentView.setAdapter(new MockedDataAdapter(this));
+    mSamples.setLayoutManager(new LinearLayoutManager(this));
+    mSamples.setAdapter(new ActivitiesAdapter(this));
   }
 
-  @Override public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.menu_footer, menu);
-    return true;
-  }
+  private static class ActivitiesAdapter extends RecyclerView.Adapter<ActivityViewHolder> {
 
-  @Override public boolean onOptionsItemSelected(MenuItem item) {
-    int id = item.getItemId();
-    if (id == R.id.action_show) {
-      mFooterBar.setVisibility(View.VISIBLE);
-      return true;
+    private final SampleActivity[] mActivities = new SampleActivity[] {
+        new SampleActivity(FooterActivity.class, "Footer Sample")
+    };
+    private final Context mContext;
+    private final LayoutInflater mInflater;
+
+    private ActivitiesAdapter(Context context) {
+      mContext = context;
+      mInflater = LayoutInflater.from(context);
     }
-    return super.onOptionsItemSelected(item);
+
+    @Override public ActivityViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+      View view = mInflater.inflate(R.layout.content_item, parent, false);
+      return new ActivityViewHolder(view);
+    }
+
+    @Override public void onBindViewHolder(ActivityViewHolder holder, int position) {
+      final SampleActivity activity = mActivities[position];
+      holder.mActivityName.setText(activity.getActivityName());
+      holder.mActivityName.setOnClickListener(new View.OnClickListener() {
+        @Override public void onClick(View v) {
+          mContext.startActivity(new Intent(mContext, activity.getActivityClass()));
+        }
+      });
+    }
+
+    @Override public int getItemCount() {
+      return mActivities.length;
+    }
   }
 
-  @OnClick(R.id.footer_bar) public void hideFooter() {
-    mFooterBar.setVisibility(View.GONE);
+  public static final class ActivityViewHolder extends RecyclerView.ViewHolder {
+
+    @Bind(R.id.text_content) TextView mActivityName;
+
+    public ActivityViewHolder(View itemView) {
+      super(itemView);
+      ButterKnife.bind(this, itemView);
+    }
   }
 }
